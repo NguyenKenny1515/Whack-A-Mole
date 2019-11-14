212ecc06dfd5ab071e8c0856a683bcd8f666f63c
mdeely9 <54382140+mdeely9@users.noreply.github.com>
2019-11-13 17:14:15 -0800

    Update SceneComponent.java

--- SceneComponent.java
+++ SceneComponent.java
@@ -1,30 +1,61 @@
-import java.awt.*;
-import javax.swing.*;
-import java.util.*;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+import java.util.TimerTask;
+
+import javax.management.timer.Timer;
+import javax.swing.JComponent;
 
 /**
- * A component that shows a scene composed of shapes that is displayed by Frame.
- */
-public class SceneComponent extends JComponent {
-
+   Used from Textbook Code
+*/
+public class SceneComponent extends JComponent
+{
 	private ArrayList<GrowableShape> shapes;
 	
-	public SceneComponent() {
+	public SceneComponent()
+	{
 		shapes = new ArrayList<>();
 	}
-
-	/**
-	 Adds a shape to the scene.
-	 @param s the shape to add
-	 */
-	public void add(GrowableShape s) {
+   
+	public void add(GrowableShape s)
+	{
 		shapes.add(s);
 		repaint();
 	}
+	
+	public int score;
 
-	public void paintComponent(Graphics g) {
+	public int time = 60;
+	
+
+	Timer timer = new Timer();
+	/*TimerTask task = new TimerTask() {
+		public void run() {
+			time--;
+			timer.scheduleAtFixedRate(task,1000,1000);
+		}
+	};*/
+	
+	
+
+
+		   
+   public void paintComponent(Graphics g)
+   {
       Graphics2D g2 = (Graphics2D) g;
+      g.setFont(new Font("Arial", Font.PLAIN, 30));
+      g.drawString("SCORE: " + score, 1, 30);
+      g.setFont(new Font("Arial", Font.PLAIN, 30));
+      g.drawString("TIMER: " + time, 1750 , 30);
+ 
+  
+      
+  
       for (GrowableShape s : shapes)
+      {
          s.draw(g2);
+      }
    }
 }
